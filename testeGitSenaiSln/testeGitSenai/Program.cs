d3d1395a00269087fc4e1d5123a583c9c42ead6e
jrhoinski <jrhoinski@gmail.com>
2019-10-03 14:33:51 -0300

    primeiro commit teste

---
+++ testeGitSenaiSln/testeGitSenai/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace testeGitSenai
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("primeiro teste..");
+            Console.ReadKey();
+        }
+    }
+}
